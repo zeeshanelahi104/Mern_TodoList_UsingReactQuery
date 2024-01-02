b93c8d04aba850d1b7096121a6ea0c846e1b9ca8
zeebeecoding <zeeshanelahi104@gmail.com>
2024-01-02 16:40:05 +0500

    index.js file updated

--- Server/index.js
+++ Server/index.js
@@ -4,40 +4,34 @@ const cors = require("cors");
 
 require("dotenv").config();
 
-// create app
+// Create app
 const app = express();
 
-// cors
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-// routes
+// Routes
 const todoRoutes = require("./src/routes/todoRoutes");
 app.use("/", todoRoutes);
 
 // Mongodb connection
-mongoose.connect(process.env.CONNECTION_STRING, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+async function connectToDatabase() {
+  try {
+    await mongoose.connect(process.env.CONNECTION_STRING, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-const db = mongoose.connection;
-// ===========================Mongodb Connection Scenarios==============================
-//          1-                                               2-
-//  Handle control if your                        Handle control if your db
-//  db not connected successfuly                      connected successfuly
+    console.log("MongoDB connected successfully!");
+    const port = process.env.PORT || 5000;
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+  }
+}
 
-// 1- scenario when mongodb not connected
-db.on("error", (error) => {
-  console.log("Mongodb error : ", error);
-});
-
-// 2- scenario when mongodb connected
-
-db.once("open", () => {
-  console.log("Mongodb connected sucessfully!");
-  const port = process.env.PORT;
-  app.listen(port, () => {
-    console.log(`server is running on ${port} port`);
-  });
-});
+// Connect to MongoDB
+connectToDatabase();
